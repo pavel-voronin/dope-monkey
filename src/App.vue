8aa588a5a1fb0ca5e309c1280d3ee44e2623fab5
Pavel Voronin <pavel.a.voronin@gmail.com>
2023-11-09 00:33:52 +0700

    more like start of the project

--- src/App.vue
+++ src/App.vue
@@ -9,5 +9,11 @@ increment()
 </script>
 
 <template>
-  <div class="text-red-500" @click="increment">{{ count }}</div>
-</template>
+  <div class="w-full h-full flex flex-col justify-center items-center gap-4">
+    <div class="text-4xl">Hello, <a
+        class="selection:bg-green-600 underline selection:text-white decoration-green-600 decoration-8"
+        href="https://www.gwars.io">GWars.io</a></div>
+    <div
+      class="border px-4 py-2 select-none bg-slate-700 shadow-lg hover:shadow-xl hover:-translate-y-1 cursor-pointer transition-all text-white rounded-md text-xl"
+      @click="increment">Counter: {{ count }}</div>
+</div></template>
--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -2,7 +2,7 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 
 export const useCounterStore = defineStore("counter", () => {
-  const count = ref(5);
+  const count = ref(0);
 
   function increment() {
     count.value++;
